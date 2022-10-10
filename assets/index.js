d81774976d49ee2fd780c1adc072b082174357a9
Uvais Ahmad <uvss21@gmail.com>
2022-10-11 00:38:12 +0530

    Adding Styling and some Funct on fav/Unfav

--- assets/index.js
+++ assets/index.js
@@ -2,11 +2,25 @@
 let url = 'https://www.superheroapi.com/api.php/1168930280669000';
 let container = document.querySelector('.container');
 
-for(let id=1 ; id<40 ; id++){
-	fetchDataAsync(id);
+fetchAndLoadListner();
+
+//FeTCH all data and then it load Event Listner on all Favourite btn
+async function fetchAndLoadListner(){
+	for(let id=1 ; id<40 ; id++){
+		await fetchDataAsync(id);
+	}
+	let favBtn = document.getElementsByClassName('fav-btn')
+	
+	//apply eventListner on Each Favicon
+	for(f of favBtn){
+		console.log('This is Fav',f)
+		await f.addEventListener('click',addToFavourite);
+	}
 }
 
 
+
+//Fech single Data using the ID
 async function fetchDataAsync(id){
 
 	await fetch(`${url}/${id}`)
@@ -16,35 +30,37 @@ async function fetchDataAsync(id){
 
 		let item = `<div class="card item h-50 m-4 hero" style="width: 13rem;">
 						<img src=${data.image.url} class="card-img-top thumbnail" alt="...">
-						<div class="card-body">
+						<div class="card-body d-flex flex-row justify-content-around">
 							<h5 class="card-title text-center"><a class="text-decoration-none" href=assets/showDetails.html?id=${data.id}>${data.name}</a></h5>							    
+							<div class="fav-btn" >
+								<img class="favImg" id="${data.id}"  src="assets/images/unFav.png" alt="Unfav" >
+							</div>
 						</div>
 					</div>`
-			// container.appendChild(item);
+			
 			container.innerHTML += item
+
+			
 		})
 	.catch(error => console.error('Error while fetching req : ',error))
-
+			
+	
 }
 
-	//This is JQuery Code i removed it with vanilla JS
+//To adding in the favourite
+async function addToFavourite(e){
+	
+	await e.target.parentElement.removeEventListener('click',addToFavourite);
+	e.target.src="assets/images/fav.png"
+	e.target.alt="favourite"
+	await e.target.parentElement.addEventListener('click',removeFromFavourite)
+}
 
-	// await $.ajax({
-	// 	type : 'get',
-	// 	url : `${url}/${id}`,
-	// 	success : function(data ){
-	// 		// console.log('Data : ',data);
-	// 		// console.log('Full Name :',data.biography)
-		
-	// 		let item = `<div class="card item h-50 m-4 hero" style="width: 13rem;">
-	// 						<img src=${data.image.url} class="card-img-top thumbnail" alt="...">
-	// 						<div class="card-body">
-	// 							<h5 class="card-title text-center"><a class="text-decoration-none" href=assets/showDetails.html?id=${data.id}>${data.name}</a></h5>							    
-	// 						</div>
-	// 					</div>`
-	// 		$('.container').append(item);
-	// 	},
-	// 	error : function(err){
-	// 		console.log('Error : ',err);
-	// 	}
-	// })
+// to removing from the favourite
+async function removeFromFavourite(e){
+	
+	await e.target.parentElement.removeEventListener('click',removeFromFavourite);
+	e.target.src="assets/images/unFav.png"
+	e.target.alt="Unfav"
+	await e.target.parentElement.addEventListener('click',addToFavourite)
+}
